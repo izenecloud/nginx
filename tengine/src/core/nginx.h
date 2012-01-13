4476e3df6662cdfaaeff3e764c00db7b8bb192fe
Paolo D'Apice <paolo.dapice@izenesoft.com>
2012-01-13 14:02:18 +0800

    switched to Taobao's tengine (customized nginx)

---
+++ tengine/src/core/nginx.h
@@ -0,0 +1,23 @@
+
+/*
+ * Copyright (C) Igor Sysoev
+ */
+
+
+#ifndef _NGINX_H_INCLUDED_
+#define _NGINX_H_INCLUDED_
+
+
+#define nginx_version      1000011
+#define NGINX_VERSION      "1.0.11"
+#define NGINX_VER          "nginx/" NGINX_VERSION
+
+#define tengine_version    1002002
+#define TENGINE_VERSION    "1.2.2"
+#define TENGINE_VER        "Tengine/" TENGINE_VERSION
+
+#define NGINX_VAR          "NGINX"
+#define NGX_OLDPID_EXT     ".oldbin"
+
+
+#endif /* _NGINX_H_INCLUDED_ */
