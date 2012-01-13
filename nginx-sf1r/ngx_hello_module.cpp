b9064f58605f9e7785de6147a39b3b3d1bb47df6
Paolo D'Apice <paolo.dapice@izenesoft.com>
2012-01-13 13:53:55 +0800

    added integer parameter to hello module; C++ cast instructions

--- nginx-sf1r/ngx_hello_module.cpp
+++ nginx-sf1r/ngx_hello_module.cpp
@@ -13,9 +13,15 @@ extern "C" {
 }
 #include <string>
 
+
+#define CAST(T,V)       static_cast< T >( V )
+#define HELLO           "Ciao! 你好！"
+
+
 /// Location configuration structure.
 typedef struct {
-    ngx_str_t hello_str;
+    ngx_uint_t hello_uint; ///< test int 
+    ngx_str_t  hello_str;  ///< test string
 } ngx_hello_loc_conf_t;
 
 
@@ -34,6 +40,14 @@ static ngx_command_t ngx_hello_commands[] = {
         NGX_HTTP_LOC_CONF_OFFSET,            // configuration
         0,                                   // offset
         NULL                                 // post
+    },
+    {
+        ngx_string("hello_uint"),
+        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+        ngx_conf_set_num_slot,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_hello_loc_conf_t, hello_uint),
+        NULL,
     },
     {
         ngx_string("hello_str"),
@@ -65,17 +79,15 @@ static ngx_http_module_t ngx_hello_module_ctx = {
 
 static void* 
 ngx_hello_create_loc_conf(ngx_conf_t* cf) {
-    ngx_hello_loc_conf_t* conf;
-
     // allocate module struct
-    conf = (ngx_hello_loc_conf_t*) ngx_pcalloc(cf->pool, sizeof(ngx_hello_loc_conf_t));
+    ngx_hello_loc_conf_t* conf = CAST(ngx_hello_loc_conf_t*, ngx_pcalloc(cf->pool, sizeof(ngx_hello_loc_conf_t)));
     if (conf == NULL) {
         return NGX_CONF_ERROR;
     }
     // init struct values
-    //conf->hello_str = ngx_string("");
+    conf->hello_uint = NGX_CONF_UNSET_UINT;
     conf->hello_str.len = 0;
-    conf->hello_str.data = (u_char*) "";
+    conf->hello_str.data = CAST(u_char*, NGX_CONF_UNSET_PTR);
 
     return conf;
 }
@@ -83,11 +95,12 @@ ngx_hello_create_loc_conf(ngx_conf_t* cf) {
 
 static char* 
 ngx_hello_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child) {
-    ngx_hello_loc_conf_t* prev = (ngx_hello_loc_conf_t*) parent;
-    ngx_hello_loc_conf_t* conf = (ngx_hello_loc_conf_t*) child;
+    ngx_hello_loc_conf_t* prev = CAST(ngx_hello_loc_conf_t*, parent);
+    ngx_hello_loc_conf_t* conf = CAST(ngx_hello_loc_conf_t*, child);
 
-    // merge values
-    ngx_conf_merge_str_value(conf->hello_str, prev->hello_str, ""); // default to 1
+    // merge values (with defaults)
+    ngx_conf_merge_uint_value(conf->hello_uint, prev->hello_uint, 0);
+    ngx_conf_merge_str_value(conf->hello_str, prev->hello_str, "");
 
     return NGX_CONF_OK;
 }
@@ -110,61 +123,57 @@ ngx_module_t ngx_hello_module = {
 };
 
 
-// useful strings
-static const std::string helloString = "Ciao! 你好！";
-
-
 static ngx_int_t
 ngx_hello_handler(ngx_http_request_t* request) {
-    ngx_int_t rc;       // return code
-    ngx_buf_t* buffer;
-    ngx_chain_t out;
-    
-    ngx_hello_loc_conf_t* conf;
-    conf = (ngx_hello_loc_conf_t*) ngx_http_get_module_loc_conf(request, ngx_hello_module);
+    //ngx_hello_loc_conf_t* conf = CAST(ngx_hello_loc_conf_t*, ngx_http_get_module_loc_conf(request, ngx_hello_module));
 
     // response to 'GET' and 'POST' requests only
     if (!(request->method & (NGX_HTTP_GET|NGX_HTTP_POST))) {
         return NGX_HTTP_NOT_ALLOWED;
     }
 
-    /* set resonse header */
+    /* do actual processing */
+    std::string helloString(HELLO);
+    helloString += 1;
     
-    //request->headers_out.content_type_len = sizeof("text/html") - 1;
+    /* set response header */
+    
+    // set the status line
+    request->headers_out.status = NGX_HTTP_OK;
+    request->headers_out.content_length_n = helloString.length();
+    request->headers_out.content_type_len = sizeof("text/html") - 1;
     request->headers_out.content_type.len = sizeof("text/html") - 1;
     request->headers_out.content_type.data = (u_char*) "text/html";
-    
-    // send the header
-    rc = ngx_http_send_header(request);
     
     /* set response body */
     
     // allocate a buffer
-    buffer = (ngx_buf_t*) ngx_pcalloc(request->pool, sizeof(ngx_buf_t));
+    ngx_buf_t* buffer = CAST(ngx_buf_t*, ngx_pcalloc(request->pool, sizeof(ngx_buf_t)));
     if (buffer == NULL) {
         ngx_log_error(NGX_LOG_ERR, request->connection->log, 0, "Failed to allocate response buffer.");
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
     }
-
-    // attach this buffer to the buffer chain
-    out.buf = buffer;
-    out.next = NULL;
-
+    
+    u_char* temp = (u_char*) helloString.c_str();
+    
     // adjust the pointers of the buffer
-    buffer->pos = (u_char*) helloString.c_str();                              // first position of the data
-    buffer->last = (u_char*) (helloString.c_str() + helloString.length());    // last position of the data
+    buffer->pos = temp;                              // first position of the data
+    buffer->last = temp + helloString.length();      // last position of the data
     buffer->memory = 1;                                             // this buffer is in memory (read-only)
     buffer->last_buf = 1;                                           // this is the last buffer in the buffer chain
 
-    // set the status line
-    request->headers_out.status = NGX_HTTP_OK;
-    request->headers_out.content_length_n = helloString.length();
+    // attach this buffer to the buffer chain
+    ngx_chain_t out;
+    out.buf = buffer;
+    out.next = NULL;
 
-    
+    // send the header
+    ngx_int_t rc = ngx_http_send_header(request);
+
     if (rc == NGX_ERROR || rc > NGX_OK || request->header_only) {
         return rc;
     }
-
+    
     // send the buffer chain
     return ngx_http_output_filter(request, &out);
 }
@@ -172,9 +181,8 @@ ngx_hello_handler(ngx_http_request_t* request) {
 
 static char*
 ngx_hello(ngx_conf_t* cf, ngx_command_t* cmd, void* conf) {
-    ngx_http_core_loc_conf_t* clcf;
-
-    clcf = (ngx_http_core_loc_conf_t*) ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module); // get the core struct for this location
+    // get the core struct for this location
+    ngx_http_core_loc_conf_t* clcf = CAST(ngx_http_core_loc_conf_t*, ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module)); 
     clcf->handler = ngx_hello_handler; // handler to process the 'hello' directive
 
     return NGX_CONF_OK;
