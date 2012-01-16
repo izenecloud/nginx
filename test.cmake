90173aa760c38030ca8320f03d17fd5dfc127a78
Paolo D'Apice <paolo.dapice@izenesoft.com>
2012-01-16 16:12:03 +0800

    renamed directory: nginx-sf1r -> nginx-hello-module

--- test.cmake
+++ test.cmake
@@ -1,3 +1,3 @@
 # run Test::Nginx
 set(ENV{TEST_NGINX_BINARY} ${NGINX})
-execute_process(COMMAND ${PROVE} -I${LIB} -I${INC} WORKING_DIRECTORY ${DIR})
+execute_process(COMMAND ${PROVE} -v -I${LIB} -I${INC} WORKING_DIRECTORY ${DIR})
