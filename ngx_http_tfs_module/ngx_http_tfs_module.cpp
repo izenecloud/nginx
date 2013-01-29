692e3dda4110e7f86a21aa075c05700f2782684a
lscm <lscm@B5M-S1.(none)>
2013-01-29 14:35:35 -0500

    add watermark support for image

--- ngx_http_tfs_module/ngx_http_tfs_module.cpp
+++ ngx_http_tfs_module/ngx_http_tfs_module.cpp
@@ -17,8 +17,9 @@ using namespace Magick;
 #define DEFAULT_TFS_READ_WRITE_SIZE (2 * 1024 * 1024)
 #define ZOOMPARAM_LEN 64
 #define QUALITY_LEN 10
+#define WATERMARK_LEN 6
 #define scast(T,V)                      static_cast< T >( (V) )
-#define TFS_NS_ARRAY_INIT_SIZE             4
+#define TFS_NS_ARRAY_INIT_SIZE             8
 
 static void* ngx_http_tfs_create_main_conf(ngx_conf_t *cf);
 static void* ngx_http_tfs_create_loc_conf(ngx_conf_t *cf);
@@ -26,10 +27,12 @@ static char* ngx_http_tfs_merge_loc_conf(ngx_conf_t *cf, void *parent, void *chi
 static char* ngx_http_tfs_get(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_tfs_init_process(ngx_cycle_t* cycle);
 static void ngx_tfs_exit_process(ngx_cycle_t* cycle);
+static bool get_arg_value(ngx_http_request_t *r, const u_char* args_str, int find_offset, const ngx_str_t* arg_key, int maxlen, u_char* arg_value);
 
 typedef struct {
     ngx_str_t tfs_nsip; 		
     size_t tfs_rb_buffer_size;
+    ngx_str_t watermark_file;
     TfsClient* tfsclient;
 } ngx_http_tfs_ns_loc_conf_t;
 
@@ -58,6 +61,14 @@ static ngx_command_t  ngx_http_tfs_commands[] = {
       NGX_HTTP_LOC_CONF_OFFSET,
       offsetof(ngx_http_tfs_ns_loc_conf_t, tfs_rb_buffer_size),
       NULL },
+
+    { ngx_string("watermark_file"),
+      NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
+      ngx_conf_set_str_slot	,            	/* 直接调用内置的字符串解释函数解释参数*/
+      NGX_HTTP_LOC_CONF_OFFSET,         	/* 只在location中配置 */
+      offsetof(ngx_http_tfs_ns_loc_conf_t, watermark_file),
+      NULL },
+
 
       ngx_null_command
 };
@@ -94,15 +105,16 @@ ngx_module_t  ngx_http_tfs_module = {
 static ngx_int_t 
 ngx_tfs_init_process(ngx_cycle_t* cycle) {
 
-    ngx_log_error(NGX_LOG_INFO, cycle->log, 0, "init tfs handle process");
     InitializeMagick(NULL);   
 
     ngx_http_tfs_ns_main_conf_t* main_conf = scast(ngx_http_tfs_ns_main_conf_t*, ngx_http_cycle_get_module_main_conf(cycle, ngx_http_tfs_module));
     ngx_http_tfs_ns_loc_conf_t** loc_confs = scast(ngx_http_tfs_ns_loc_conf_t**,
         main_conf->loc_confs.elts);
 
+    ngx_log_error(NGX_LOG_INFO, cycle->log, 0, "init tfs handle process, tfs conf num:%d", main_conf->loc_confs.nelts);
     for( ngx_uint_t i = 0; i < main_conf->loc_confs.nelts; i++ )
     {
+        ngx_log_error(NGX_LOG_INFO, cycle->log, 0, "tfs conf init for:%s.", loc_confs[i]->tfs_nsip.data);
         if(loc_confs[i] == NULL)
         {
             ngx_log_error(NGX_LOG_ERR, cycle->log, 0, "location conf is null.");
@@ -114,7 +126,7 @@ ngx_tfs_init_process(ngx_cycle_t* cycle) {
         {
             ngx_log_error(NGX_LOG_ERR, cycle->log, 0, "init tfsclient failed.");
             loc_confs[i]->tfsclient = NULL;
-            return NGX_OK;
+            continue;
         }
     }
     TBSYS_LOGGER.setLogLevel("WARN");
@@ -126,14 +138,45 @@ ngx_tfs_exit_process(ngx_cycle_t* cycle) {
     ngx_log_error(NGX_LOG_INFO, cycle->log, 0, "exit tfs handle process");
 }
 
+static bool get_arg_value(ngx_http_request_t *r, const u_char* args_str, int find_offset, const ngx_str_t* arg_key, int maxlen, u_char* arg_value)
+{
+    arg_value[0] = '\0';
+    const char* key_pos = ngx_strstr(args_str + find_offset, arg_key->data);
+    if(key_pos != NULL)
+    {
+        const char* separate_char = ngx_strchr( key_pos + arg_key->len, '&');
+        int key_param_len = r->args.len - (key_pos - (const char*)args_str + arg_key->len) + 1;
+        ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "find key_pos:%s, %zu", key_pos, r->args.len);
+        if(separate_char != NULL)
+        {
+            key_param_len = separate_char - ((const char*)key_pos + arg_key->len) + 1;
+        }
+        if(key_param_len > maxlen)
+        {
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "key %s param too long:%d.", arg_key->data, key_param_len);
+            return false;
+        }
+        u_char* tmp = (u_char*)ngx_create_temp_buf(r->pool, key_param_len);
+        strncpy((char*)tmp, key_pos + arg_key->len, key_param_len);
+        tmp[key_param_len - 1] = '\0';
+        u_char* p = arg_value;
+        ngx_unescape_uri(&p, &tmp, key_param_len, NGX_UNESCAPE_URI);
+        arg_value[key_param_len - 1] = '\0';
+        ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "arg_key %s, value:%s.", arg_key->data, arg_value);
+        return true;
+    }
+    ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "arg_key %s not found.", arg_key->data);
+    return false;
+ }
 
 static ngx_int_t
-ngx_http_tfs_get_args_tfsname(ngx_http_request_t *r, u_char *ret, u_char* zoomparam, u_char* qualityparam)
+ngx_http_tfs_get_args_tfsname(ngx_http_request_t *r, u_char *ret, u_char* zoomparam, u_char* qualityparam, u_char* watermarkparam)
 {
 	static const ngx_str_t TFSNAME_KEY = ngx_string("filename=");
 	static const ngx_str_t ZOOMPARAM_KEY = ngx_string("zoom=");
 	static const ngx_str_t QUALITY_KEY = ngx_string("quality=");
-    static const int32_t MAX_ARGS_LEN = TFSNAME_KEY.len + ZOOMPARAM_KEY.len + QUALITY_KEY.len + TFS_FILE_LEN + 1 + ZOOMPARAM_LEN + QUALITY_LEN + 1;
+	static const ngx_str_t WATERMARK_KEY = ngx_string("watermark=");
+    static const int32_t MAX_ARGS_LEN = TFSNAME_KEY.len + ZOOMPARAM_KEY.len + QUALITY_KEY.len + WATERMARK_KEY.len + TFS_FILE_LEN + 1 + ZOOMPARAM_LEN + QUALITY_LEN + WATERMARK_LEN + 1;
     u_char args_str[MAX_ARGS_LEN];
     ngx_cpystrn(args_str, r->args.data, min(MAX_ARGS_LEN - 1, (int32_t)r->args.len + 1));
     args_str[MAX_ARGS_LEN - 1] = '\0';
@@ -159,49 +202,12 @@ ngx_http_tfs_get_args_tfsname(ngx_http_request_t *r, u_char *ret, u_char* zoompa
         }
 		ngx_cpystrn(ret, (args_str + TFSNAME_KEY.len), tfs_filename_len);
         ret[tfs_filename_len - 1] = '\0';
-        zoomparam[0] = '\0';
-        const char* zoom_pos = ngx_strstr(args_str + TFSNAME_KEY.len + tfs_filename_len, ZOOMPARAM_KEY.data);
-        if(zoom_pos != NULL)
+        bool get_arg_ret = get_arg_value(r, args_str, TFSNAME_KEY.len + tfs_filename_len, &ZOOMPARAM_KEY, ZOOMPARAM_LEN, zoomparam);
+        if (get_arg_ret)
         {
-            const char* separate_char = ngx_strchr( zoom_pos + ZOOMPARAM_KEY.len, '&');
-            int zoom_param_len = r->args.len - (zoom_pos - (const char*)args_str + ZOOMPARAM_KEY.len) + 1;
-            ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "tfs zoom param zoom_pos:%s, %zu", zoom_pos, r->args.len);
-            if(separate_char != NULL)
+            get_arg_ret = get_arg_value(r, args_str, TFSNAME_KEY.len + tfs_filename_len, &QUALITY_KEY, QUALITY_LEN, qualityparam);
+            if(get_arg_ret)
             {
-                zoom_param_len = separate_char - ((const char*)zoom_pos + ZOOMPARAM_KEY.len) + 1;
-            }
-            if(zoom_param_len > ZOOMPARAM_LEN)
-            {
-                ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "tfs zoom param too long:%d.", zoom_param_len);
-                return NGX_ERROR;
-            }
-            u_char* tmp = (u_char*)ngx_create_temp_buf(r->pool, zoom_param_len);
-            //strncpy((char*)zoomparam, zoom_pos + ZOOMPARAM_KEY.len, zoom_param_len);
-            strncpy((char*)tmp, zoom_pos + ZOOMPARAM_KEY.len, zoom_param_len);
-            tmp[zoom_param_len - 1] = '\0';
-            u_char* p = zoomparam;
-            ngx_unescape_uri(&p, &tmp, zoom_param_len, NGX_UNESCAPE_URI);
-            zoomparam[zoom_param_len - 1] = '\0';
-            const char* quality_pos = ngx_strstr(args_str + TFSNAME_KEY.len + tfs_filename_len + ZOOMPARAM_KEY.len + zoom_param_len, QUALITY_KEY.data);
-            if(quality_pos != NULL)
-            {
-                const char* separate_char = ngx_strchr( quality_pos + QUALITY_KEY.len, '&');
-                int quality_param_len = r->args.len - (quality_pos - (const char*)args_str + QUALITY_KEY.len) + 1;
-                if(separate_char != NULL)
-                {
-                    quality_param_len = separate_char - (quality_pos + QUALITY_KEY.len) + 1;
-                }
-                if(quality_param_len > QUALITY_LEN)
-                {
-                    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "tfs get file quality param too long:%d.", quality_param_len);
-                    return NGX_ERROR;
-                }
-                u_char* tmp2 = (u_char*)ngx_create_temp_buf(r->pool, quality_param_len);
-                strncpy((char*)tmp2, quality_pos + QUALITY_KEY.len, quality_param_len);
-                tmp2[quality_param_len - 1] = '\0';
-                u_char* p = qualityparam;
-                ngx_unescape_uri(&p, &tmp2, zoom_param_len, NGX_UNESCAPE_URI);
-                qualityparam[quality_param_len - 1] = '\0';
                 ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "tfs get file with quality: %s.",  qualityparam);
             }
             else
@@ -213,7 +219,12 @@ ngx_http_tfs_get_args_tfsname(ngx_http_request_t *r, u_char *ret, u_char* zoompa
         {
             ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "tfs get file without zoom.");
         }
-        ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "tfs get filename:%s, zoom param:%s.", ret, zoomparam);
+        get_arg_ret = get_arg_value(r, args_str, TFSNAME_KEY.len + tfs_filename_len, &WATERMARK_KEY, WATERMARK_LEN, watermarkparam);
+        if (get_arg_ret)
+        {
+            ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "tfs get file with watermark.");
+        }
+        ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "tfs get filename:%s, zoom param:%s, watermarkparam:%s", ret, zoomparam, watermarkparam);
 		return NGX_OK;
 	}
     ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "tfs get args failed 2. %s, %zu, %zu.", args_str, r->args.len, TFSNAME_KEY.len);
@@ -230,6 +241,7 @@ ngx_http_tfs_get_handler(ngx_http_request_t *r)
     u_char tfsname[TFS_FILE_LEN + 1];
     u_char zoomparam[ZOOMPARAM_LEN + 1] = {'\0'};
     u_char qualityparam[QUALITY_LEN + 1] = {'\0'};
+    u_char watermarkparam[WATERMARK_LEN + 1] = {'\0'};
     ngx_http_tfs_ns_loc_conf_t  *cglcf;
 
     cglcf = (ngx_http_tfs_ns_loc_conf_t*)ngx_http_get_module_loc_conf(r, ngx_http_tfs_module);
@@ -242,7 +254,7 @@ ngx_http_tfs_get_handler(ngx_http_request_t *r)
         return NGX_HTTP_NOT_MODIFIED;
     }
 
-    if( NGX_OK != ngx_http_tfs_get_args_tfsname(r, tfsname, zoomparam, qualityparam)) {
+    if( NGX_OK != ngx_http_tfs_get_args_tfsname(r, tfsname, zoomparam, qualityparam, watermarkparam)) {
         ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "reject tfs get request for wrong args. ");
     	return NGX_HTTP_NOT_ALLOWED;
     }
@@ -250,6 +262,7 @@ ngx_http_tfs_get_handler(ngx_http_request_t *r)
     int fd = -1;
     if(cglcf->tfsclient == NULL)
     {
+        ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "tfs ns: %s", cglcf->tfs_nsip.data);
         cglcf->tfsclient = TfsClient::Instance();
         int ret = cglcf->tfsclient->initialize((const char*)cglcf->tfs_nsip.data);
         if(ret != TFS_SUCCESS)
@@ -314,7 +327,7 @@ ngx_http_tfs_get_handler(ngx_http_request_t *r)
 	}
 
     //static const ngx_str_t good_str = ngx_string("good");
-    if(ngx_strlen(zoomparam) > 0)
+    if(ngx_strlen(zoomparam) > 0 || ngx_strlen(watermarkparam) > 0)
     {
         Blob imgdata;
         imgdata.update((char*)b->pos, finfo.size_);
@@ -325,6 +338,30 @@ ngx_http_tfs_get_handler(ngx_http_request_t *r)
         try
         {
             image.read(imgdata);
+
+            if (ngx_strlen(watermarkparam) > 0 && cglcf->watermark_file.len > 0)
+            {
+                ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "watermark file:%s", cglcf->watermark_file.data);
+                // read watermark image.
+                Image watermark;
+                watermark.read((const char*)cglcf->watermark_file.data);
+                // 
+                //#define ForgetGravity		0
+                //#define NorthWestGravity	1
+                //#define NorthGravity		2
+                //#define NorthEastGravity	3
+                //#define WestGravity		4
+                //#define CenterGravity		5
+                //#define EastGravity		6
+                //#define SouthWestGravity	7
+                //#define SouthGravity		8
+                //#define SouthEastGravity	9
+                //#define StaticGravity		10
+
+                ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "watermark size :%d, %d", watermark.columns(), watermark.rows());
+                image.composite(watermark, SouthEastGravity, OverCompositeOp);
+            }
+
             Geometry zoom_param_geo((const char*)zoomparam);
             if(zoom_param_geo <= image.size())
             {
@@ -347,12 +384,8 @@ ngx_http_tfs_get_handler(ngx_http_request_t *r)
                 {
                     image.scale(zoom_param_geo);
                 }
-                image.write(&zoomed_imgdata);
             }
-            else
-            {
-                zoomed_imgdata = imgdata;
-            }
+            image.write(&zoomed_imgdata);
         }
         catch( Magick::Exception &e)
         {
@@ -372,7 +405,8 @@ ngx_http_tfs_get_handler(ngx_http_request_t *r)
             resize_error = true;
             //return NGX_HTTP_INTERNAL_SERVER_ERROR;
         }
-        if(resize_error || ((int)zoomed_imgdata.length() > (int)finfo.size_) )
+        if(resize_error || 
+            (ngx_strlen(zoomparam) > 0 && ((int)zoomed_imgdata.length() > (int)finfo.size_)) )
         {
             if(!resize_error)
             {
@@ -382,6 +416,19 @@ ngx_http_tfs_get_handler(ngx_http_request_t *r)
         }
         else
         {
+            if (zoomed_imgdata.length() > finfo.size_)
+            {
+                b = (ngx_buf_t *)ngx_create_temp_buf(r->pool, zoomed_imgdata.length());
+                if (b == NULL) {
+                    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Failed to allocate response buffer: %u.\n", finfo.size_);
+                    return NGX_HTTP_INTERNAL_SERVER_ERROR;
+                }
+
+                b->last = b->pos + zoomed_imgdata.length() ;
+                b->memory = 1;
+                b->last_buf = 1;
+            }
+
             ngx_copy(b->pos, zoomed_imgdata.data(), zoomed_imgdata.length());
             r->headers_out.content_length_n = zoomed_imgdata.length();
             b->last = b->pos + zoomed_imgdata.length();
@@ -472,6 +519,7 @@ ngx_http_tfs_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
     ngx_conf_merge_str_value(conf->tfs_nsip, prev->tfs_nsip, "127.0.0.1:8108");
     ngx_conf_merge_size_value(conf->tfs_rb_buffer_size, prev->tfs_rb_buffer_size, (size_t)DEFAULT_TFS_READ_WRITE_SIZE);
 
+    ngx_conf_merge_str_value(conf->watermark_file, prev->watermark_file, "");
     ngx_conf_merge_ptr_value(conf->tfsclient, prev->tfsclient, NULL);
 
     // add to the main conf struct
